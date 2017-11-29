e27e10550c73ae4db79520475862b4f8b97e93b2
nohour <vadim@khaikin.biz>
2017-11-29 12:12:55 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+var os = require('os')
+
+app.get('/about', function (req, res) {
+	let str = 'Its server ' + os.hostname() + 'Hello world'
+	res.send(str);
+	
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function () {
+	console.log('Express server started on port ' + PORT + '!');
+});
